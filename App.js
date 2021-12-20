af9e4d2f9ed72d26d895278327d6763831ccbc79
Gim128 <gimhanpabasara4@gmail.com>
2021-12-20 09:58:38 +0530

    init

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/native-stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
